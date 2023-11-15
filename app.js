a25ce67ef938301baa00ee2e2819f14f6b683728
kmumladze <ketevanmumladze17@gmail.com>
2023-11-15 16:39:11 +0100

    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let start = document.querySelector('.btn-start');
-// let allDiv = document.querySelectorAll('.color');
+let allDiv = document.querySelectorAll('.color');
 let rgbNum = document.querySelector('.btn-rgb')
 let failOrSuc = document.querySelector('.btn-failandsucsess')
 
@@ -11,54 +11,53 @@ let boxes2 = document.querySelectorAll('.boxes2');
 let remove = document.querySelectorAll('.remove1');
 let append = document.querySelectorAll('.append1');
 
+let gamedifficulty = false //hard
 // debugger; 
 
-
-
-
-
-
 easy.addEventListener('click', function(){
+    //make game diffulty easy
+    gamedifficulty = true;
     easy.style.backgroundColor = 'green';
     easy.style.fontSize = '20px';
     hard.style.fontSize = '10px';
     remove.forEach(el=>
-        el.remove());
+        el.style.display = 'none');
+    isGame()
     
-});
+})
 
 
+// start.addEventListener('click', function(){
 
+function isGame(){
+  
 
-
-
-start.addEventListener('click', function(){
-
-    let allDiv = document.querySelectorAll('.color');
-    
     failOrSuc.innerHTML = '...';
     failOrSuc.style.backgroundColor = 'yellow';
     failOrSuc.style.color = 'black';
     let colors = [];
 
-
-    let i = 0;
+ 
     // debugger;
-    allDiv.forEach(item =>{
-        item.style.opacity = 1;
+    for(let i = 0; i < allDiv.length; i++)
+    {
+        if(gamedifficulty){
+            if (i == 3){
+                break;
+            }
+        }
+        let item = allDiv[i];
+        
         item.style.backgroundColor = getRgbColors();
-        colors[i] = (item.style.backgroundColor);
-        i++;
+        colors.push(item.style.backgroundColor);
+        
         item.addEventListener('click', function(){
             if(item.style.backgroundColor == rgbNum.innerHTML){
                 failOrSuc.innerHTML = 'Success';
                 failOrSuc.style.backgroundColor = 'green';
                 failOrSuc.style.color = 'white';
 
-                // setTimeout(function() {
-                // alert('Congrats! You won!');
-                // }, 1);
-                
+            
 
                 Swal.fire({
                 position: "center",
@@ -66,7 +65,7 @@ start.addEventListener('click', function(){
                 title: "Congrats!",
                 text: "You won!",
                 showConfirmButton: false,
-                timer: 1500
+                timer: 2000
                 });
             }
             else{
@@ -78,18 +77,22 @@ start.addEventListener('click', function(){
                 
             }
         }
+
         )
     }
+    // allDiv.forEach(item =>{
+        
+    // }
     
-        )
+    //     )
+        
         let randNum = Math.round(Math.random() * colors.length);
         rgbNum.innerHTML = colors[randNum];
         console.log(colors);
 
     
-})
-
-
+// })
+}
 function getRgbColors(){
         let r = Math.round(Math.random() * 255);
         let g = Math.round(Math.random() * 255);
@@ -102,30 +105,35 @@ function getRgbColors(){
 
 
 
-
     hard.addEventListener('click', function(){
+        gamedifficulty = false;
         hard.style.backgroundColor = 'red';
         hard.style.fontSize = '20px';
         easy.style.fontSize = '10px';
-       
+        
+        remove.forEach(el =>{
+            el.style.display = 'block';
+        });
+            
+        isGame()
     
-        Swal.fire({
-            title: "Are you sure?",
-            text: "This is hard mode!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, I want to play!"
-          }).then((result) => {
-            if (result.isConfirmed) {
-              Swal.fire({
-                title: "Let's go!",
-                text: "You can click 'ok' and then 'start'. Good luck!.",
-                // icon: "success"
-              });
-            }
-          });
+    //     Swal.fire({
+    //         title: "Are you sure?",
+    //         text: "This is hard mode!",
+    //         icon: "warning",
+    //         showCancelButton: true,
+    //         confirmButtonColor: "#3085d6",
+    //         cancelButtonColor: "#d33",
+    //         confirmButtonText: "Yes, I want to play!"
+    //       }).then((result) => {
+    //         if (result.isConfirmed) {
+    //           Swal.fire({
+    //             title: "Let's go!",
+    //             text: "You can click 'ok' and then 'start'. Good luck!.",
+    //             // icon: "success"
+    //           });
+    //         }
+    //       });
        
      
         
